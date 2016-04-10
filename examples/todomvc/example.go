10af8ba2fd676a159e13f36752a7720a8bea5773
David Brophy <dave@brophy.uk>
2016-04-10 14:24:17 +0100

    Replaced all referenced to gopherjs/vecty with davelondon/vecty

--- examples/todomvc/example.go
+++ examples/todomvc/example.go
@@ -4,13 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"github.com/davelondon/vecty"
+	"github.com/davelondon/vecty/examples/todomvc/actions"
+	"github.com/davelondon/vecty/examples/todomvc/components"
+	"github.com/davelondon/vecty/examples/todomvc/dispatcher"
+	"github.com/davelondon/vecty/examples/todomvc/store"
+	"github.com/davelondon/vecty/examples/todomvc/store/model"
 	"github.com/gopherjs/gopherjs/js"
-	"github.com/gopherjs/vecty"
-	"github.com/gopherjs/vecty/examples/todomvc/actions"
-	"github.com/gopherjs/vecty/examples/todomvc/components"
-	"github.com/gopherjs/vecty/examples/todomvc/dispatcher"
-	"github.com/gopherjs/vecty/examples/todomvc/store"
-	"github.com/gopherjs/vecty/examples/todomvc/store/model"
 )
 
 func main() {
